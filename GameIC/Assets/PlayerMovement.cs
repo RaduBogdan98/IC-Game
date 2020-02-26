9c6ba5736acccf1bbecc1ae7c18a4e1586260169
RaduBogdan98 <raducotisel@yahoo.com>
2020-02-26 17:41:19 +0200

    Project Begining, main character and animation and tiles added

---
+++ GameIC/Assets/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    private float horizontalMovement;
+    public float speed = 40;
+    private bool jump = false;
+    private bool crouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * 40;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetBool("Jump", true);
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+            animator.SetBool("Crouch", true);
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+            animator.SetBool("Crouch", false);
+        }
+    }
+
+    //Move character
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMovement * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("Jump", false);
+    }
+}
