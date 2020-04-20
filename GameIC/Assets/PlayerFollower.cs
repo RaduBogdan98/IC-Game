8c9d42f59e929504b054e1ebbbbea21c9aa0b628
Radu Cotisel <raducotisel@yahoo.com>
2020-04-20 17:00:54 +0300

    Camera tweak and jumping level 2 bug fixed

---
+++ GameIC/Assets/PlayerFollower.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.123f;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        this.transform.position = target.position + offset;
+    }
+}
