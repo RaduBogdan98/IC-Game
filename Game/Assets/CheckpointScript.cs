8845659a086fb805f4f0e7486b9aaaafc37e29bb
corneavlad98 <48709398+corneavlad98@users.noreply.github.com>
2020-07-05 13:56:17 +0300

    Added checkpoint animation

--- Game/Assets/CheckpointScript.cs
+++ Game/Assets/CheckpointScript.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class CheckpointScript : MonoBehaviour
 {
     //[SerializeField] private Transform respawnPoint;
+    public Animator animator;
     private GameMaster gm;
     private int passCounter;
     void Start()
     {
+        animator = GetComponent<Animator>();
         gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
         passCounter = 0;
     }
@@ -17,11 +19,12 @@ public class CheckpointScript : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
-        {
+        {           
             passCounter++;
             if(passCounter == 1)
             {
                 SoundManagerScript.PlaySound("sfx_sound_neutral9");
+                animator.SetTrigger("shouldShine");
             }
             //respawnPoint.position = this.transform.position;
             gm.lastCheckPointPos = transform.position;
